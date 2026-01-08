efb6a65353cf23e9f900f1b94f9e095ab1a97b6c
Blue-Onion <rawatadityasingh963@gmail.com>
2026-01-08 10:31:27 +0530

    Auth done

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -4,6 +4,7 @@ export const registerSchema = z.object({
   name: z.string().max(100, 'Name must be less than 100 characters').optional(),
   email: z.string().email('Invalid email address').min(1, 'Email is required'),
   password: z.string().min(8, 'Password must be at least 8 characters').max(100, 'Password must be less than 100 characters'),
+  
 });
 
 export const loginSchema = z.object({
