4d36bd97ed6845c4781663a8a87caf9141a86c24
Blue-Onion <rawatadityasingh963@gmail.com>
2026-01-08 11:40:03 +0530

    routes

--- src/routes/org.route.js
+++ src/routes/org.route.js
@@ -1,3 +1,4 @@
+import { gettingAllOrg, gettingOrg, creatingOrg, joiningOrg } from "#src/controllers/org.controller.js";
 import express from "express"
 
 const router=express.Router();
@@ -7,3 +8,4 @@ router.get("/get-org/:id", gettingOrg);
 router.post("/create-org", creatingOrg);
 router.post("/join-org/:id", joiningOrg);
 
+export default router
